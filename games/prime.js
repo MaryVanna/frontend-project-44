2cca9c14cbfa3b798cbfe210e5f0975c2d1d630c
MaryVanna <MashFijhoa95@yandex.ru>
2023-02-07 15:04:11 +0300

    edit prime.js

--- games/prime.js
+++ games/prime.js
@@ -13,9 +13,11 @@ const isPrime = (num) => {
   return 'yes';
 };
 
+const message = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
 const primeGame = () => {
   const userName = greeting();
-  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+  console.log(message);
   for (let i = 0; i < numberOfGames; i += 1) {
     const number = getRandomNumber();
     console.log(`Question: ${number}`);
