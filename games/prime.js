7b8462bbed9ecc111aa12ab387cf75c723f50ab0
MaryVanna <MashFijhoa95@yandex.ru>
2023-02-07 15:02:02 +0300

    edit prime.js

--- games/prime.js
+++ games/prime.js
@@ -17,10 +17,10 @@ const primeGame = () => {
   const userName = greeting();
   console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
   for (let i = 0; i < numberOfGames; i += 1) {
-    const guessedNumber = getRandomNumber();
-    console.log(`Question: ${guessedNumber}`);
+    const number = getRandomNumber();
+    console.log(`Question: ${number}`);
     const userAnswer = readlineSync.question('Your answer: ');
-    const rightAnswer = isPrime(guessedNumber);
+    const rightAnswer = isPrime(number);
     if (userAnswer.toLowerCase() !== rightAnswer) {
       return `'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.\nLet's try again, ${userName}!`;
     }
