e41aeb1d89c7ed11819ebfdf1389a4cb74ace707
MaryVanna <MashFijhoa95@yandex.ru>
2023-02-20 09:37:19 +0300

    edit index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,11 @@
 import readlineSync from 'readline-sync';
 
-export const numberOfGames = 3;
-
-export const greeting = () => {
-  console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}`);
-  return userName;
-};
-
 export const startGame = (questionAndAnswer, descr) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}`);
   console.log(descr);
-  for (let i = 0; i < numberOfGames; i += 1) {
+  for (let i = 0; i < 3; i += 1) {
     const [question, answer] = questionAndAnswer();
     console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
